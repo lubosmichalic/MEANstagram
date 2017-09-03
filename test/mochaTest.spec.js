cd6e18eb0c2a2cdeea2d26ed658f3420304011f8
Lubos Michalic <lnmichalic@gmail.com>
2017-09-03 14:32:02 +0100

    TDD our TDD

---
+++ test/mochaTest.spec.js
@@ -0,0 +1,8 @@
+var chai = require ('chai')
+var expect = chai.expect
+
+describe('UnitTestShouldWork', function() {
+  it('1+1 should equal 2', function() {
+    expect(1 + 1).to.equal(2)
+  });  
+})
